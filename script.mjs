11780a7bd30342a73cb5df1eb989da0876b724b4
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-08-20 15:14:03 -0400

    Using class to create Characters

--- script.mjs
+++ script.mjs
@@ -1,22 +1,29 @@
-// Humble Beginnings
-
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword, potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "cat",
-        companion: {
-            name: "Frank",
-            type: "Flea",
-            belongings: ["small hat", "sunglasses"]
-        }
-    },
+// Class Fantasy
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+        this.companion = {
+            name: this.name,
+            type: this.type,
+            inventory: this.inventory
+        };
+    }
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
     }
 }
 
-adventurer.roll()
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion,", "artifact"];
+const leo = robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+const frank = robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+
+leo.roll();
+frank.roll();
